65effc3f6fce5da2af0c201bcf48901642e5f680
MartinJeanne <martinjeanne.dev@gmail.com>
2024-08-03 14:32:20 +0200

    work in progress for input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,17 +2,26 @@ import { useState } from 'react'
 import '../styles/Input.css'
 
 import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { Button } from 'primereact/button';
 
 export default function Input() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState<number>(0)
     const [value, setValue] = useState('Coucou')
 
     return (
         <div className='input'>
-            <h1>Investment tracker</h1>
             <InputText value={value} onChange={(e) => setValue(e.target.value)} />
-            <button className='submit' onClick={() => setCount((count) => count + 1)}>Submit {count}</button>
+            
+            <div className='test'>
+                <InputNumber value={count} onValueChange={(e) => setValue(e.value)} useGrouping={false} showButtons mode="decimal" />
+            </div>
 
+            <div className='test'>
+                <InputNumber inputId="currency-germany" value={count} onValueChange={(e) => setValue(e.value)} showButtons mode="currency" mode="currency" currency="EUR" locale="de-DE" />
+            </div>
+
+            <Button className='submit' label="submit" onClick={() => setCount((count) => count + 1)} />
         </div>
     )
 }
